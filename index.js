dbbcbdf139b542ed9fe5b85b225a61701eccd30f
chandra <101719497+chandra1899@users.noreply.github.com>
2024-06-27 20:31:57 +0530

    change socket structure

--- index.js
+++ index.js
@@ -16,72 +16,16 @@ const io = new Server(httpServer, {
   },
 });
 
-const findDocument = async (id)=>{
-  if(id == null) return 
-  try {
-    let res = await fetch('http://localhost:8000/api/roomdetails',{
-      method:'POST',
-      headers:{
-        'Access-Control-Allow-Origin': '*',
-        Accept:"application/json",
-        "Content-Type":"application/json"
-      },
-      credentials:'include',
-      body:JSON.stringify({
-        roomName : id
-      })
-    })
-    let data = await res.json();
-    // console.log('data',data);
-    if(res.status === 200)
-    return data.document
-  return {}
-    
-  } catch (error) {
-    console.log("error", error);
-  }
-}
-
-const updateDocument = async (id, content)=>{
-  console.log(content);
-  if(id == null) return 
-  try {
-    let res = await fetch('http://localhost:8000/api/savedocument',{
-      method:'POST',
-      headers:{
-        'Access-Control-Allow-Origin': '*',
-        Accept:"application/json",
-        "Content-Type":"application/json"
-      },
-      credentials:'include',
-      body:JSON.stringify({
-        roomName : id,content
-      })
-    })
-    if(res.status === 200)
-      console.log('saved');
-    
-  } catch (error) {
-    console.log("error", error);
-  }
-}
-
 io.on("connection",async  (socket) => {
     console.log("A user connected:", socket.id);
   
-    socket.on('get_document',async (id)=>{
-      const document = await findDocument(id)
-      if(document == undefined) return 
+    socket.on('establish-conection',async (id)=>{
+      console.log('establish-conection', id);
       socket.join(id)
-      let d = document.content
-      // console.log('document from get_document', d);
-      socket.emit("load_document", d)
+      socket.emit("connection-established",{})
       socket.on("send-changes", async (delta) => {
-        socket.broadcast.to(id).emit('recieved_changes', delta)
-      });
-      socket.on("save_document", async (data) => {
-        // console.log('data',data);
-        await updateDocument(id, data)
+        console.log('changes sent', delta);
+        socket.broadcast.to(id).emit('receive-changes', delta)
       });
     })
 });
