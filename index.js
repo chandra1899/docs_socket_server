fb9c75c514a7e59f7a9e4e852026116d569f6ed8
chandra <101719497+chandra1899@users.noreply.github.com>
2024-07-01 00:15:56 +0530

    updated urls to avoid cors error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const httpServer = http.createServer();
   
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:8000", "https://admin.socket.io", "https://personal-document.vercel.app/"], // Replace with your frontend URL
+    origin: ["http://localhost:8000", "https://admin.socket.io", "https://personal-document.vercel.app/","https://personal-docs-142jxchn3-chandra1899s-projects.vercel.app/","https://hoppscotch.io/"], // Replace with your frontend URL
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
@@ -16,7 +16,7 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection",async  (socket) => {
-  console.log('user connected', socket);
+  console.log('user connected', socket.id);
   socket.on('establish-conection',async (id)=>{
       console.log('connection established in room', id);
       socket.join(id)
@@ -25,12 +25,12 @@ io.on("connection",async  (socket) => {
         socket.broadcast.to(id).emit('receive-changes', delta)
       });
       socket.on('disconnect', () => {
-        console.log('user leaft the room', id, 'with socket', socket);
+        console.log('user leaft the room', id, 'with socket id', socket.id);
         socket.leave(id)
       })
     })
 });
 
-httpServer.listen(process.env.PORT || 3001, () => {
-  console.log(`Socket.io server is running on port ${process.env.PORT || 3001}`);
+httpServer.listen(3001, () => {
+  console.log(`Socket.io server is running on port ${3001}`);
 });
